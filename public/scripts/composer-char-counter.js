fd3a5f0b57b4eed46a09ebc245355efb659598c8
Spencer Ang <boomerandzapper@gmail.com>
2017-05-17 00:47:41 +0000

    Added counter functionality and made an error class which changes text to red.

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,12 @@
-$( document ).ready(function() {
-    console.log( "ready!" );
+$(function(){
+  $('.new-tweet > form').on('keyup', 'textarea', function () {
+    let charused = $(this).val().length;
+    let counter = $(this).closest('form').find('.counter')
+    counter.text(140 - charused);
+    if (charused > 140) {
+      counter.addClass('error');
+    } else {
+      counter.removeClass('error');
+    }
+  });
 });
