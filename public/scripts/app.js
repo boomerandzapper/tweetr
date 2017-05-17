bfdbed0149dd733a4e5b6eeb31acc70f1286651f
Spencer Ang <boomerandzapper@gmail.com>
2017-05-17 22:45:40 +0000

    Made it so all tweets don't refresh when the user submits a new one. Also added timestamps to show in english.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -7,15 +7,19 @@
 // Test / driver code (temporary). Eventually will get this from the server.
 
 
-
+function timeSince(date) {
+  const currentDate = new Date(date)
+  return currentDate;
+}
 function createTweetElement(tweetData) {
   const header = $('<header>').append($('<img>', {src:tweetData.user.avatars.large})).append($('<h3>').text(tweetData.user.name)).append($('<span>').addClass('user-tag').text(tweetData.user.handle));
-  const footer = $('<footer>').append($('<span>').text(tweetData.created_at));
+  const footer = $('<footer>').append($('<span>').text(timeSince(tweetData.created_at)));
   const content = $('<div>').addClass('tweet-content').text(tweetData.content.text);
   return $('<article>').addClass('tweet').append(header).append(content).append(footer);
 }
 
 function renderTweets(data) {
+  $('#tweets-container').empty();
   data.forEach(function(element) {
     $('#tweets-container').append(createTweetElement(element))
   });
@@ -31,7 +35,14 @@ function loadTweets() {
   })
 }
 
-
+$(function(){
+  $('#nav-bar > button').on('click', function () {
+    $('.new-tweet').slideToggle(350, function() {
+      $(this).find('textarea').focus();
+      console.log('dfd');
+    });
+  })
+});
 
 $(function(){
   loadTweets();
@@ -52,7 +63,6 @@ $(function(){
         url: '/tweets',
         data: form.serialize()
       }).done(function() {
-        $('#tweets-container').empty();
         loadTweets();
       });
       text.val('');
